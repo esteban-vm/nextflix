da4583b118965f5c17352c647398ddf2669748ec
esteban-vm <estebanvm90@outlook.com>
2024-11-01 13:25:11 -0300

    add constants

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+import type { NavigationItemProps } from '@/shared/navigation-item'
+
+export const navigationItems: NavigationItemProps[] = [
+  { name: 'Inicio', link: '/' },
+  { name: 'Películas', link: '/' },
+  { name: 'Series', link: '/' },
+  { name: 'Mi lista', link: '/' },
+]
