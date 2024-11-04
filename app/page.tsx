c9d40cda9fea9218de4d7e7ffdbebfce026eed22
esteban-vm <estebanvm90@outlook.com>
2024-11-03 22:07:12 -0300

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import { SliderVideo } from '@/(home)/components'
-import { Navigation } from '@/components/shared'
 
 export default function HomePage() {
   return (
-    <section className='relative'>
-      <Navigation />
+    <>
       <SliderVideo />
-    </section>
+    </>
   )
 }
