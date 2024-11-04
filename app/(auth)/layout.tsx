2641c42b684895860e2b68220410dda47f5c5056
esteban-vm <estebanvm90@outlook.com>
2024-11-03 22:07:35 -0300

    update root layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,18 +1,18 @@
 import type { ReactNode } from 'react'
 import Image from 'next/image'
-import { Logo } from '@/components/shared'
 import loginBg from '@/images/login-bg.jpg'
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className='relative size-full'>
-      <Image alt='login-bg' className='object-cover object-center opacity-40' src={loginBg} fill />
-      <div className='absolute left-1/2 w-full max-w-screen-2xl -translate-x-1/2 p-4 2xl:px-0'>
-        <Logo />
+    <>
+      <div className='relative size-full'>
+        <Image alt='login-bg' className='object-cover object-center opacity-30' src={loginBg} fill />
+      </div>
+      <div className='absolute left-1/2 top-1/2 w-full max-w-screen-2xl -translate-x-1/2 -translate-y-1/2'>
         <div className='mx-auto max-w-md text-center'>
           <div>{children}</div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
