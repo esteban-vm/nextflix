3705df007d50d03154a528cf9cd6e78c179c92a4
esteban-vm <estebanvm90@outlook.com>
2024-11-04 22:01:10 -0300

    fixing styles for auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,9 +8,9 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
       <div className='relative size-full'>
         <Image alt='login-bg' className='object-cover object-center opacity-30' src={loginBg} fill />
       </div>
-      <div className='absolute left-1/2 top-1/2 w-full max-w-screen-2xl -translate-x-1/2 -translate-y-1/2'>
-        <div className='mx-auto max-w-md text-center'>
-          <div>{children}</div>
+      <div className='absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2'>
+        <div className='mx-auto max-w-[90%] rounded-xl bg-background/80 ~px-10/12 ~py-12/14 sm:max-w-lg'>
+          {children}
         </div>
       </div>
     </>
