908f8a975073f4103fafdd45ef5b0e70c3de1e2d
esteban-vm <estebanvm90@outlook.com>
2024-11-04 22:00:33 -0300

    refactors

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import type { ElementRef, ComponentPropsWithoutRef } from 'react'
 import { Root, Indicator } from '@radix-ui/react-checkbox'
 import { CheckIcon } from '@radix-ui/react-icons'
@@ -10,14 +8,15 @@ export const Checkbox = forwardRef<ElementRef<typeof Root>, ComponentPropsWithou
   ({ className, ...props }, ref) => (
     <Root
       ref={ref}
+      checked='indeterminate'
       className={cn(
         'peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
         className
       )}
       {...props}
     >
-      <Indicator className={cn('flex items-center justify-center text-current')}>
-        <CheckIcon className='size-4' />
+      <Indicator className='flex items-center justify-center text-current'>
+        <CheckIcon />
       </Indicator>
     </Root>
   )
--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -1,19 +1,16 @@
+/* eslint-disable react/function-component-definition */
+
 import type { VariantProps } from 'class-variance-authority'
 import type { ComponentPropsWithoutRef, ElementRef, HTMLAttributes } from 'react'
-import * as Dialog from '@radix-ui/react-dialog'
+import { Close, Portal as SheetPortal, Overlay, Content, Title, Description } from '@radix-ui/react-dialog'
 import { Cross2Icon } from '@radix-ui/react-icons'
 import { cva } from 'class-variance-authority'
 import { forwardRef } from 'react'
 import { cn } from '@/lib/utils'
 
-const Sheet = Dialog.Root
-const SheetTrigger = Dialog.Trigger
-const SheetClose = Dialog.Close
-const SheetPortal = Dialog.Portal
-
-const SheetOverlay = forwardRef<ElementRef<typeof Dialog.Overlay>, ComponentPropsWithoutRef<typeof Dialog.Overlay>>(
+const SheetOverlay = forwardRef<ElementRef<typeof Overlay>, ComponentPropsWithoutRef<typeof Overlay>>(
   ({ className, ...props }, ref) => (
-    <Dialog.Overlay
+    <Overlay
       className={cn(
         'fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
         className
@@ -24,7 +21,7 @@ const SheetOverlay = forwardRef<ElementRef<typeof Dialog.Overlay>, ComponentProp
   )
 )
 
-SheetOverlay.displayName = Dialog.Overlay.displayName
+SheetOverlay.displayName = Overlay.displayName
 
 const sheetVariants = cva(
   'fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500 data-[state=open]:animate-in data-[state=closed]:animate-out',
@@ -45,63 +42,50 @@ const sheetVariants = cva(
   }
 )
 
-type SheetContentProps = ComponentPropsWithoutRef<typeof Dialog.Content> & VariantProps<typeof sheetVariants>
+type SheetContentProps = ComponentPropsWithoutRef<typeof Content> & VariantProps<typeof sheetVariants>
 
-const SheetContent = forwardRef<ElementRef<typeof Dialog.Content>, SheetContentProps>(
-  ({ side = 'right', className, children, ...props }, ref) => (
-    <SheetPortal>
-      <SheetOverlay />
-      <Dialog.Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
-        <Dialog.Close className='absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary'>
-          <Cross2Icon className='size-4' />
-          <span className='sr-only'>Close</span>
-        </Dialog.Close>
-        {children}
-      </Dialog.Content>
-    </SheetPortal>
-  )
-)
-
-SheetContent.displayName = Dialog.Content.displayName
-
-function SheetHeader({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn('flex flex-col space-y-2 text-center sm:text-left', className)} {...props} />
-}
-
-SheetHeader.displayName = 'SheetHeader'
-
-function SheetFooter({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2', className)} {...props} />
-}
-
-SheetFooter.displayName = 'SheetFooter'
-
-const SheetTitle = forwardRef<ElementRef<typeof Dialog.Title>, ComponentPropsWithoutRef<typeof Dialog.Title>>(
-  ({ className, ...props }, ref) => {
-    return <Dialog.Title ref={ref} className={cn('text-lg font-semibold text-foreground', className)} {...props} />
+const SheetContent = forwardRef<ElementRef<typeof Content>, SheetContentProps>(
+  ({ side = 'right', className, children, ...props }, ref) => {
+    return (
+      <SheetPortal>
+        <SheetOverlay />
+        <Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
+          <Close className='absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary'>
+            <Cross2Icon className='size-4' />
+            <span className='sr-only'>Close</span>
+          </Close>
+          {children}
+        </Content>
+      </SheetPortal>
+    )
   }
 )
 
-SheetTitle.displayName = Dialog.Title.displayName
+SheetContent.displayName = Content.displayName
 
-const SheetDescription = forwardRef<
-  ElementRef<typeof Dialog.Description>,
-  ComponentPropsWithoutRef<typeof Dialog.Description>
->(({ className, ...props }, ref) => (
-  <Dialog.Description ref={ref} className={cn('text-sm text-muted-foreground', className)} {...props} />
-))
-
-SheetDescription.displayName = Dialog.Description.displayName
-
-export {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetOverlay,
-  SheetPortal,
-  SheetTitle,
-  SheetTrigger,
+const SheetHeader = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => {
+  return <div className={cn('flex flex-col space-y-2 text-center sm:text-left', className)} {...props} />
 }
+
+const SheetFooter = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => {
+  return <div className={cn('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2', className)} {...props} />
+}
+
+const SheetTitle = forwardRef<ElementRef<typeof Title>, ComponentPropsWithoutRef<typeof Title>>(
+  ({ className, ...props }, ref) => {
+    return <Title ref={ref} className={cn('text-lg font-semibold text-foreground', className)} {...props} />
+  }
+)
+
+SheetTitle.displayName = Title.displayName
+
+const SheetDescription = forwardRef<ElementRef<typeof Description>, ComponentPropsWithoutRef<typeof Description>>(
+  ({ className, ...props }, ref) => {
+    return <Description ref={ref} className={cn('text-sm text-muted-foreground', className)} {...props} />
+  }
+)
+
+SheetDescription.displayName = Description.displayName
+
+export { Root as Sheet, Close as SheetClose, Trigger as SheetTrigger } from '@radix-ui/react-dialog'
+export { SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetOverlay, SheetPortal, SheetTitle }
