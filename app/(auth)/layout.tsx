5863605a06c69766b9b145823e7113c000fd3006
esteban-vm <estebanvm90@outlook.com>
2024-11-05 16:59:41 -0300

    update auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,7 +9,7 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
         <Image alt='login-bg' className='object-cover object-center opacity-30' src={loginBg} fill />
       </div>
       <div className='absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2'>
-        <div className='mx-auto max-w-[90%] rounded-xl bg-background/80 ~px-10/12 ~py-12/14 sm:max-w-lg'>
+        <div className='mx-auto flex max-w-[90%] flex-col justify-center gap-2 rounded-xl bg-background/80 ~px-10/12 ~py-12/14 sm:max-w-lg'>
           {children}
         </div>
       </div>
