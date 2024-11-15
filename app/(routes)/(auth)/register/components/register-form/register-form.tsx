43a57b2c7535324f353ac9b61b948667f0bf7fb9
esteban-vm <estebanvm90@outlook.com>
2024-11-15 10:53:14 -0300

    create custom hooks for auth forms

--- app/(routes)/(auth)/register/components/register-form/register-form.tsx
+++ app/(routes)/(auth)/register/components/register-form/register-form.tsx
@@ -1,51 +1,11 @@
 'use client'
 
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useHookFormAction } from '@next-safe-action/adapter-react-hook-form/hooks'
-import { useRouter } from 'next/navigation'
 import { AuthField, AuthButton } from '@/(auth)/components'
-import { register } from '@/actions'
+import { useRegisterForm } from '@/(auth)/hooks'
 import { Form } from '@/components/ui'
-import { toast } from '@/hooks'
-import { registerSchema } from '@/lib/validations'
 
 export function RegisterForm() {
-  const { push } = useRouter()
-
-  const { form, handleSubmitWithAction, resetFormAndAction } = useHookFormAction(
-    register,
-    zodResolver(registerSchema),
-    {
-      actionProps: {
-        onSuccess() {
-          push('/login')
-          toast({ title: 'Te has registrado correctamente', description: '¡Bienvenido/a!' })
-        },
-        onError({ error }) {
-          toast({ title: error.validationErrors?._errors?.[0], variant: 'destructive' })
-        },
-        onSettled() {
-          resetFormAndAction()
-        },
-        onExecute() {
-          toast({ title: 'Creando cuenta', description: 'Un momento…' })
-        },
-      },
-      formProps: {
-        mode: 'onChange',
-        defaultValues: {
-          email: '',
-          password: '',
-          repeatPassword: '',
-        },
-      },
-    }
-  )
-
-  const {
-    control,
-    formState: { isSubmitting },
-  } = form
+  const { form, control, isSubmitting, handleSubmitWithAction } = useRegisterForm()
 
   return (
     <Form {...form}>
