7a4a5f81b8a6442b9fe100333c70b81e074a4533
esteban-vm <estebanvm90@outlook.com>
2025-01-08 12:24:28 -0300

    update global type

--- types/globals.d.ts
+++ types/globals.d.ts
@@ -1,8 +1,11 @@
 import type { LoginSchema, ProfileSchema, RegisterSchema } from '@/lib/validations'
 import type { User as UserType, Movie as MovieType, Profile as ProfileType } from 'prisma/prisma-client'
+import type { PropsWithChildren } from 'react'
 import type { z } from 'zod'
 
 declare global {
+  type WithChildren<P = unknown> = Readonly<Required<PropsWithChildren<P>>>
+
   namespace Validations {
     type Login = z.infer<typeof LoginSchema>
     type Profile = z.infer<typeof ProfileSchema>
