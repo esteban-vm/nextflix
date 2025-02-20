293c7ced041ee47cac5c793b19f9ed63cc6c914b
Esteban V.M <estebanvm90@outlook.com>
2025-02-20 10:31:32 -0300

    working on home ui

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,33 @@ import { verifySession } from '@/lib/auth'
 export default async function HomePage() {
   await verifySession()
 
-  const trendingMovies = await MovieActions.findTrending()
-  const playingMovies = await MovieActions.findPlaying()
-
-  console.log({ trendingMovies: trendingMovies?.data })
-  console.log({ playingMovies: playingMovies?.data })
+  const trendingMovieResults = await MovieActions.findTrending()
+  const trendingMovies = trendingMovieResults?.data ?? []
 
   return (
-    <HomeUI.PageWrapper>
-      <HomeUI.BackgroundVideo />
-      <HomeUI.InnerWrapper>
-        <HomeUI.PageTitle>Test</HomeUI.PageTitle>
-        <HomeUI.MainText />
-        <HomeUI.ButtonWrapper>
-          <HomeUI.PlayButton />
-          <HomeUI.InfoButton />
-        </HomeUI.ButtonWrapper>
-      </HomeUI.InnerWrapper>
-      <HomeUI.BackgroundGradient />
-    </HomeUI.PageWrapper>
+    <>
+      <HomeUI.Hero>
+        <HomeUI.BackgroundVideo />
+        <HomeUI.CTA>
+          <HomeUI.CTATitle>Test</HomeUI.CTATitle>
+          <HomeUI.CTAText />
+          <HomeUI.ButtonWrapper>
+            <HomeUI.PlayButton />
+            <HomeUI.InfoButton />
+          </HomeUI.ButtonWrapper>
+        </HomeUI.CTA>
+        <HomeUI.BackgroundGradient />
+      </HomeUI.Hero>
+      <HomeUI.Trending>
+        <HomeUI.TrendingTitle>Las series más populares hoy en tu país:</HomeUI.TrendingTitle>
+        <HomeUI.TrendingList>
+          {trendingMovies.map((movie) => (
+            <HomeUI.TrendingCard key={movie.id} movie={movie}>
+              <HomeUI.TrendingPlayer movie={movie} />
+            </HomeUI.TrendingCard>
+          ))}
+        </HomeUI.TrendingList>
+      </HomeUI.Trending>
+    </>
   )
 }
