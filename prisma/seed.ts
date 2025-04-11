159623856d795a08cc477f26f803e4bf6dc108eb
Esteban V.M. <estebanvm90@outlook.com>
2025-04-11 09:07:44 -0300

    update prisma models

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,16 @@
-import type { AvatarUrl } from '@/lib/constants'
 import type { Prisma } from '@prisma/client'
 import { hashSync } from 'bcryptjs'
+import { avatarUrls } from '@/lib/constants'
 import { db } from '@/lib/db'
 import { movies, users } from '@/prisma/data'
 
 async function cleanDataBase() {
-  const args: Prisma.UserWhereInput = { email: { not: { equals: 'test@email.com' } } }
+  const input: Prisma.UserWhereInput = { email: { not: { equals: 'test@email.com' } } }
 
-  await db.favoriteMovie.deleteMany({ where: { user: args } })
-  await db.movie.deleteMany({ where: { favorites: { every: { user: args } } } })
-  await db.profile.deleteMany({ where: { user: args } })
-  await db.user.deleteMany({ where: args })
+  await db.favoriteMovie.deleteMany({ where: { profile: { user: input } } })
+  await db.movie.deleteMany({ where: { favoriteMovies: { every: { profile: { user: input } } } } })
+  await db.profile.deleteMany({ where: { user: input } })
+  await db.user.deleteMany({ where: input })
 }
 
 async function insertInitialData() {
@@ -26,7 +26,7 @@ async function insertInitialData() {
   await db.movie.createMany({ data: [...movies.playing, ...movies.trending] })
 }
 
-async function insertRelationalData() {
+async function insertAdditionalData() {
   const user1 = await db.user.findUniqueOrThrow({ where: { email: users[0].email } })
 
   const [profile1, profile2] = await db.profile.createManyAndReturn({
@@ -38,8 +38,6 @@ async function insertRelationalData() {
     ],
   })
 
-  await db.currentProfile.create({ data: { userId: user1.id, profileId: profile1.id } })
-
   const movie1 = await db.movie.findUniqueOrThrow({ where: { title: movies.playing[0].title } })
   const movie2 = await db.movie.findUniqueOrThrow({ where: { title: movies.playing[2].title } })
   const movie3 = await db.movie.findUniqueOrThrow({ where: { title: movies.playing[5].title } })
@@ -47,24 +45,14 @@ async function insertRelationalData() {
 
   await db.favoriteMovie.createMany({
     data: [
-      { userId: user1.id, movieId: movie1.id },
-      { userId: user1.id, movieId: movie2.id },
-      { userId: user1.id, movieId: movie3.id },
-      { userId: user1.id, movieId: movie4.id },
-    ],
-  })
-
-  const avatar1: AvatarUrl = '/images/avatars/avatar1.webp'
-  const avatar2: AvatarUrl = '/images/avatars/avatar2.webp'
-  const avatar3: AvatarUrl = '/images/avatars/avatar3.webp'
-  const avatar4: AvatarUrl = '/images/avatars/avatar4.webp'
-
-  await db.profile.createMany({
-    data: [
-      { name: 'Sergio', avatarUrl: avatar1, userId: user1.id },
-      { name: 'Clara', avatarUrl: avatar2, userId: user1.id },
-      { name: 'Carlos', avatarUrl: avatar3, userId: user1.id },
-      { name: 'Ana', avatarUrl: avatar4, userId: user1.id },
+      { profileId: profile1.id, movieId: movie1.id },
+      { profileId: profile1.id, movieId: movie2.id },
+      { profileId: profile1.id, movieId: movie3.id },
+      { profileId: profile1.id, movieId: movie4.id },
+      { profileId: profile2.id, movieId: movie4.id },
+      { profileId: profile2.id, movieId: movie3.id },
+      { profileId: profile2.id, movieId: movie2.id },
+      { profileId: profile2.id, movieId: movie1.id },
     ],
   })
 }
@@ -74,7 +62,7 @@ void (async function () {
     console.log('Seeding started')
     await cleanDataBase()
     await insertInitialData()
-    await insertRelationalData()
+    await insertAdditionalData()
     console.log('Seeding completed')
   } catch (error) {
     console.error(error)
