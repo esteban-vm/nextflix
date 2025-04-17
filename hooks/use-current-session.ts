2b49e229f93088aa93bec190795374064f46d9ab
Esteban V.M. <estebanvm90@outlook.com>
2025-04-17 10:36:57 -0300

    update current session hook

--- hooks/use-current-session.ts
+++ hooks/use-current-session.ts
@@ -5,14 +5,16 @@ import { usePathname } from 'next/navigation'
 import { getSession } from 'next-auth/react'
 import { useState, useEffect, useCallback } from 'react'
 
+type CurrentSession = Utils.Nullable<Session>
+type SessionStatus = 'authenticated' | 'loading' | 'unauthenticated'
+
 /**
  * @see {@link https://github.com/nextauthjs/next-auth/issues/9504#issuecomment-1901351841 | useSession only getting the session after manually reloading the page #9504}
  */
 
-// This hook doesn't rely on the session provider
 export const useCurrentSession = () => {
-  const [session, setSession] = useState<Session | null>(null)
-  const [status, setStatus] = useState<'authenticated' | 'loading' | 'unauthenticated'>('unauthenticated')
+  const [session, setSession] = useState<CurrentSession>(null)
+  const [status, setStatus] = useState<SessionStatus>('unauthenticated')
   const pathname = usePathname()
 
   const getCurrentSession = useCallback(async () => {
