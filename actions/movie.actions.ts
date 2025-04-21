ae5d8f9c1a008fc8bd3582c959e5bfd70d0a11c6
Esteban V.M. <estebanvm90@outlook.com>
2025-04-21 15:16:34 -0300

    add new movie action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- actions/movie.actions.ts
+++ actions/movie.actions.ts
@@ -1,9 +1,31 @@
 'use server'
 
+import { returnValidationErrors } from 'next-safe-action'
 import { cache } from 'react'
 import { toListWithPlaceholders, toPlayingMovie, toTrendingMovie } from '@/lib/adapters'
 import { db } from '@/lib/db'
+import { getPlaceholderImage } from '@/lib/images'
 import { authClient } from '@/lib/safe-action'
+import { SchemaWithID } from '@/lib/validations'
+
+export const findOneById = authClient.schema(SchemaWithID).action(
+  cache(async ({ parsedInput: { id } }): Promise<Models.Movie> => {
+    const result = await db.movie.findUnique({ where: { id } })
+
+    if (!result) {
+      returnValidationErrors(SchemaWithID, {
+        _errors: ['La película no existe'],
+      })
+    }
+
+    const movie: Models.Movie = {
+      ...result,
+      placeholder: await getPlaceholderImage(result.posterUrl),
+    }
+
+    return movie
+  })
+)
 
 export const findPlaying = authClient.action(
   cache(async (): Promise<Models.PlayingMovie[]> => {
