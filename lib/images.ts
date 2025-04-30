5e50008ae1a7e324b78bbc84601bf0639407073a
Esteban V.M. <estebanvm90@outlook.com>
2025-04-30 13:40:19 -0300

    update placeholder image fn
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- lib/images.ts
+++ lib/images.ts
@@ -1,35 +1,13 @@
 'use server'
 
-import fs from 'fs'
-import path from 'path'
-import sharp from 'sharp'
+import { getCldImageUrl } from 'next-cloudinary'
 
-async function getFileBufferRemote(url: string) {
-  const response = await fetch(url)
-  return Buffer.from(await response.arrayBuffer())
-}
-
-async function getFileBufferLocal(filepath: string) {
-  const realFilepath = path.join(process.cwd(), 'public', filepath)
-  return await fs.promises.readFile(realFilepath)
-}
-
-function getFileBuffer(src: string) {
-  const isRemote = src.startsWith('http')
-  return isRemote ? getFileBufferRemote(src) : getFileBufferLocal(src)
-}
-
-function toBase64PNG(str: string) {
-  return `data:image/png;base64,${str}`
-}
-
-export async function getPlaceholderImage(str: string) {
-  try {
-    const originalBuffer = await getFileBuffer(str)
-    const resizedBuffer = await sharp(originalBuffer).resize(20).toBuffer()
-    return toBase64PNG(resizedBuffer.toString('base64'))
-  } catch {
-    const str = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOsa2yqBwAFCAICLICSyQAAAABJRU5ErkJggg=='
-    return toBase64PNG(str)
-  }
+export async function getPlaceholderImage(src: string) {
+  const imageUrl = getCldImageUrl({ src })
+  const response = await fetch(imageUrl)
+  const arrayBuffer = await response.arrayBuffer()
+  const buffer = Buffer.from(arrayBuffer)
+  const base64 = buffer.toString('base64')
+  const image = `data:${response.type};base64,${base64}`
+  return image
 }
