14372e9b688832030cca9a2d573747276813e0f0
Esteban V.M. <estebanvm90@outlook.com>
2025-04-30 15:37:20 -0300

    refactors
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- components/common/full-image.tsx
+++ components/common/full-image.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import type { CldImageProps as DefaultCldImageProps } from 'next-cloudinary'
-import { CldImage as DefaultCldImage } from 'next-cloudinary'
+import type { CldImageProps } from 'next-cloudinary'
+import { CldImage } from 'next-cloudinary'
 
 export function FullImage({ noBlur, ...rest }: FullImageProps) {
-  return <DefaultCldImage {...rest} placeholder={noBlur ? 'empty' : 'blur'} fill />
+  return <CldImage {...rest} placeholder={noBlur ? 'empty' : 'blur'} fill />
 }
 
-export interface FullImageProps extends Omit<DefaultCldImageProps, 'placeholder' | 'fill'> {
+export interface FullImageProps extends Omit<CldImageProps, 'placeholder' | 'fill'> {
   noBlur?: boolean
 }
