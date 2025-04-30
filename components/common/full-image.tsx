a702d5035ca5bdaaf2d133383cfb9abb958f59c9
Esteban V.M. <estebanvm90@outlook.com>
2025-04-30 13:31:29 -0300

    cleanup
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- components/common/full-image.tsx
+++
@@ -1,9 +0,0 @@
-import type { ImageProps } from 'next/image'
-import Image from 'next/image'
-
-export function FullImage(props: FullImageProps) {
-  // eslint-disable-next-line jsx-a11y/alt-text
-  return <Image {...props} placeholder='blur' fill />
-}
-
-export type FullImageProps = Omit<ImageProps, 'placeholder' | 'fill'>
